7ab67dec7b0bdad109fa3d723a0ec549d569ca07
Mat Seto <seanseto@gmail.com>
2020-12-31 12:04:29 -0800

    implement mouse bounds checking

--- src/index.ts
+++ src/index.ts
@@ -25,11 +25,13 @@ const values: number[] = randomArray(COUNT, MIN, MAX)
 /* Sketch Function */
 function sketch(p: p5) {
   /* drawing parameters */
-  let itemSize = 50
-  let offsetX = itemSize
-  let offsetY = itemSize
-  let focusIndex = 0
-  let didFindValue = false
+  const state = {
+    itemSize: 50,
+    offsetX: 50,
+    offsetY: 50,
+    focusIndex: 0,
+    didFindValue: false,
+  }
 
   /* UI References */
   let doWrapCheckbox: p5.Element
@@ -60,12 +62,12 @@ function sketch(p: p5) {
     linearSearchBtn = p.createButton("Linear Search")
     linearSearchBtn.parent(buttonContainerId)
     linearSearchBtn.mouseClicked(async () => {
-      didFindValue = false
+      state.didFindValue = false
       linearSearchBtn.attribute("disabled", "true")
       /* call this function after each iteration in the linear search */
       const funct = async (index: number) => {
         /* set the index to focus on */
-        focusIndex = index
+        state.focusIndex = index
 
         /* if slider is above 0, search with a delay */
         const currentDelay = +speedSlider.value() * 10
@@ -78,8 +80,8 @@ function sketch(p: p5) {
       console.log(`${timeElapsedMs / 1000}s`)
       linearSearchBtn.removeAttribute("disabled")
 
-      if (result > -1) didFindValue = true
-      else focusIndex = -1
+      if (result > -1) state.didFindValue = true
+      else state.focusIndex = -1
     })
 
     /* BINARY SEARCH */
@@ -89,10 +91,12 @@ function sketch(p: p5) {
   const MIN_ITEM_SIZE = 30
   const MAX_ITEM_SIZE = 200
   p.mouseWheel = (event: WheelEvent) => {
-    itemSize += -0.5 * event.deltaY
-    if (itemSize < MIN_ITEM_SIZE) itemSize = MIN_ITEM_SIZE
-    if (itemSize > MAX_ITEM_SIZE) itemSize = MAX_ITEM_SIZE
-    p.textSize(itemSize / 3)
+    if (!isWithinCanvas()) return
+
+    state.itemSize += -0.5 * event.deltaY
+    if (state.itemSize < MIN_ITEM_SIZE) state.itemSize = MIN_ITEM_SIZE
+    if (state.itemSize > MAX_ITEM_SIZE) state.itemSize = MAX_ITEM_SIZE
+    p.textSize(state.itemSize / 3)
   }
 
   const DEFAULT_PAN_KEY = 32 // SPACEBAR
@@ -100,36 +104,39 @@ function sketch(p: p5) {
     p.background(204)
 
     /* canvas panning */
-    if (p.mouseIsPressed) {
-      offsetX += p.mouseX - p.pmouseX
-      offsetY += p.mouseY - p.pmouseY
+    if (p.mouseIsPressed && isWithinCanvas()) {
+      state.offsetX += p.mouseX - p.pmouseX
+      state.offsetY += p.mouseY - p.pmouseY
     }
 
     /**
      * return to default panning upon a special key press
      */
-    if (p.keyIsDown(DEFAULT_PAN_KEY)) {
-      offsetX = itemSize
-      offsetY = itemSize
+    if (p.keyIsDown(DEFAULT_PAN_KEY) && isWithinCanvas()) {
+      state.offsetX = state.itemSize
+      state.offsetY = state.itemSize
     }
 
-    p.translate(offsetX, offsetY)
+    p.translate(state.offsetX, state.offsetY)
 
     /* draw value squares */
     values.forEach((value, index) => {
       /* row wrapping */
 
-      // @ts-ignore .checked() exists on a p5.Element checkbox. No specific checkbox type exists in the p5 type definititions.
-      const itemsPerRow = getItemsPerRow(doWrapCheckbox.checked(), itemSize)
+      const itemsPerRow = getItemsPerRow(
+        // @ts-ignore .checked() exists on a p5.Element checkbox. No specific checkbox type exists in the p5 type definititions.
+        doWrapCheckbox.checked(),
+        state.itemSize,
+      )
 
-      const x = itemSize * (index % itemsPerRow)
-      const y = itemSize * Math.floor(index / itemsPerRow)
+      const x = state.itemSize * (index % itemsPerRow)
+      const y = state.itemSize * Math.floor(index / itemsPerRow)
 
       let color: ColorTuple
       let textColor: ColorTuple = [0, 0, 0]
-      if (index === focusIndex) {
+      if (index === state.focusIndex) {
         /* highlight the focused index */
-        if (didFindValue) color = [0, 255, 0]
+        if (state.didFindValue) color = [0, 255, 0]
         else color = [255, 255, 0]
       } else {
         /* default color */
@@ -138,7 +145,17 @@ function sketch(p: p5) {
         textColor = [255, 255, 255]
       }
 
-      drawItem(p, x, y, offsetX, offsetY, value, itemSize, color, textColor)
+      drawItem(
+        p,
+        x,
+        y,
+        state.offsetX,
+        state.offsetY,
+        value,
+        state.itemSize,
+        color,
+        textColor,
+      )
     })
   }
 }
@@ -160,6 +177,15 @@ const DEFAULT_ROW_SIZE = 10
 function getItemsPerRow(doWrap: boolean, itemSize?: number) {
   if (doWrap && itemSize) return Math.floor((p5Instance.width / itemSize) * 0.9)
   return DEFAULT_ROW_SIZE
+}
+
+/**
+ * @returns whether the coordinate is within the canvas or not
+ */
+function isWithinCanvas() {
+  const y = p5Instance.mouseY
+  const upperBound = visualizerDiv.offsetHeight
+  return y > 0 && y < upperBound
 }
 
 /* resize canvas */
