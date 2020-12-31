895279f01d02ac935ff23fed27beba6d0e7a3c05
Mat Seto <seanseto@gmail.com>
2020-12-31 11:51:06 -0800

    implement optional row wrapping

--- src/index.ts
+++ src/index.ts
@@ -19,37 +19,45 @@ const buttonContainerId = "buttons"
 /* Global Variables */
 const MIN = 0
 const MAX = 10000
-const SIZE = 10
-const values: number[] = randomArray(SIZE, MIN, MAX)
+const COUNT = 10
+const values: number[] = randomArray(COUNT, MIN, MAX)
 
 /* Sketch Function */
 function sketch(p: p5) {
+  /* drawing parameters */
   let itemSize = 50
   let offsetX = itemSize
   let offsetY = itemSize
   let focusIndex = 0
   let didFindValue = false
 
+  /* UI References */
+  let doWrapCheckbox: p5.Element
+  let speedSlider: p5.Element
+  let searchValue: p5.Element
+  let linearSearchBtn: p5.Element
+
+  /* CANVAS SETUP */
   p.setup = () => {
-    //
     // CONFIG
-    //
     p.createCanvas(visualizerDiv.clientWidth, visualizerDiv.clientHeight)
-    p.strokeWeight(2)
     p.frameRate(120)
 
-    //
     // UI
-    //
-    const speedSlider = p.createSlider(0, 10)
+    /* checkbox for row wrapping */
+    doWrapCheckbox = p.createCheckbox("Wrap", true)
+    doWrapCheckbox.parent(buttonContainerId)
+
+    /* slider for animation speed */
+    speedSlider = p.createSlider(0, 10)
     speedSlider.parent(buttonContainerId)
 
-    /* buttons */
+    /* buttons for selecting the operation */
     /* LINEAR SEARCH */
-    const searchValue = p.createInput("Search for...", "number")
+    searchValue = p.createInput("Search for...", "number")
     searchValue.parent(buttonContainerId)
 
-    const linearSearchBtn = p.createButton("Linear Search")
+    linearSearchBtn = p.createButton("Linear Search")
     linearSearchBtn.parent(buttonContainerId)
     linearSearchBtn.mouseClicked(async () => {
       didFindValue = false
@@ -80,28 +88,39 @@ function sketch(p: p5) {
   /* change size on scroll */
   const MIN_ITEM_SIZE = 30
   const MAX_ITEM_SIZE = 200
-  const EXPECTED_SIZE = 10
   p.mouseWheel = (event: WheelEvent) => {
     itemSize += -0.5 * event.deltaY
     if (itemSize < MIN_ITEM_SIZE) itemSize = MIN_ITEM_SIZE
     if (itemSize > MAX_ITEM_SIZE) itemSize = MAX_ITEM_SIZE
-    p.textSize((16 * EXPECTED_SIZE) / getItemsPerRow(itemSize))
+    p.textSize(itemSize / 3)
   }
 
+  const DEFAULT_PAN_KEY = 32 // SPACEBAR
   p.draw = async () => {
-    p.translate(offsetX, offsetY)
+    p.background(204)
 
+    /* canvas panning */
     if (p.mouseIsPressed) {
-      offsetX = offsetX + (p.mouseX - p.pmouseX)
-      offsetY = offsetY + (p.mouseY - p.pmouseY)
+      offsetX += p.mouseX - p.pmouseX
+      offsetY += p.mouseY - p.pmouseY
     }
 
-    p.background(204)
+    /**
+     * return to default panning upon a special key press
+     */
+    if (p.keyIsDown(DEFAULT_PAN_KEY)) {
+      offsetX = itemSize
+      offsetY = itemSize
+    }
+
+    p.translate(offsetX, offsetY)
 
     /* draw value squares */
     values.forEach((value, index) => {
       /* row wrapping */
-      const itemsPerRow = getItemsPerRow(itemSize)
+
+      // @ts-ignore .checked() exists on a p5.Element checkbox. No specific checkbox type exists in the p5 type definititions.
+      const itemsPerRow = getItemsPerRow(doWrapCheckbox.checked(), itemSize)
 
       const x = itemSize * (index % itemsPerRow)
       const y = itemSize * Math.floor(index / itemsPerRow)
@@ -109,18 +128,17 @@ function sketch(p: p5) {
       let color: ColorTuple
       let textColor: ColorTuple = [0, 0, 0]
       if (index === focusIndex) {
+        /* highlight the focused index */
         if (didFindValue) color = [0, 255, 0]
         else color = [255, 255, 0]
       } else {
+        /* default color */
         const saturation = p.map(value, MIN, MAX, 0, 255)
         color = [saturation, 0, 0]
         textColor = [255, 255, 255]
       }
 
       drawItem(p, x, y, offsetX, offsetY, value, itemSize, color, textColor)
-      p.noStroke()
-
-      p.fill(255, 255, 0, 10)
     })
   }
 }
@@ -131,16 +149,20 @@ const p5Instance = new p5(sketch, sketchContainerId)
 //
 // FUNCTIONS
 //
+
+const DEFAULT_ROW_SIZE = 10
+/**
+ * Calculate the number of squares in a row based on canvas width
+ * @param doWrap whether to enable row wrapping or not. Default row size is used if this is false
+ * @param itemSize size of each square
+ * @returns items per row based on screen width or a default value
+ */
+function getItemsPerRow(doWrap: boolean, itemSize?: number) {
+  if (doWrap && itemSize) return Math.floor((p5Instance.width / itemSize) * 0.9)
+  return DEFAULT_ROW_SIZE
+}
+
 /* resize canvas */
 window.addEventListener("resize", () => {
   p5Instance.resizeCanvas(visualizerDiv.clientWidth, visualizerDiv.clientHeight)
 })
-
-/**
- * Calculate the number of squares in a row based on canvas width
- * @param offset padding
- * @param itemSize size of each square
- */
-function getItemsPerRow(itemSize: number) {
-  return Math.floor((p5Instance.width / itemSize) * 0.9)
-}
